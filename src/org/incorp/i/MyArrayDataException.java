599d22cd3ad6022aa39b30aa373fefa1b9a2b754
Egor <ilinincorp@mail.ru>
2021-07-27 18:45:35 +0300

    Initial commit

---
+++ src/org/incorp/i/MyArrayDataException.java
@@ -0,0 +1,20 @@
+package org.incorp.i;
+
+public class MyArrayDataException extends RuntimeException {
+
+    private int row;
+    private int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public MyArrayDataException(String s, int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
---
+++ src/org/incorp/i/MyArraySizeException.java
@@ -0,0 +1,5 @@
+package org.incorp.i;
+
+public class MyArraySizeException extends RuntimeException {
+//
+}
